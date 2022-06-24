96f7495860b30554bd00ddf5810ff0cf746d6c3b
ba_c <swunhtetoo@gmail.com>
2022-06-24 10:57:08 +0630

    Learning Framer Motion

--- products/src/index.js
+++ products/src/index.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './index.css';
 
 class Application extends React.Component {
@@ -59,10 +60,17 @@ class Overview extends React.Component {
 class Navigation extends React.Component {
     render () {
         return (
-            <div>
+            <motion.div
+                drag
+                dragConstraints={{left:0,top:0, right:0, bottom:0}}
+                dragElastic={0.1}
+            >
+                <motion.svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <motion.path animate={{pathLength: 1}}  stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                </motion.svg>
                 <Link to="/app"> Application </Link> |
                 <Link to="/posts"> Overview </Link>
-            </div>
+            </motion.div>
         )
     }
 }
@@ -116,6 +124,16 @@ class Main extends React.Component {
         this.setState({
             postRoutes: this.postDetails
         })
+
+        this.containerVariants = {
+            hidden: {
+                opacity: 0
+            },
+            visible: {
+                opacity: 1,
+                x: '100px'
+            }
+        }
     }
 
     deleteBtn (obj) {
@@ -127,6 +145,16 @@ class Main extends React.Component {
     render () {
         return (
             <Router>
+                <motion.h2 
+                variants={this.containerVariants}
+                whileHover={{
+                    x: '100px', y:'10px', rotateZ:'-1deg', color: "yellow", scale: [2,1,3,2,1,1,2,3], originX:0
+                }}
+                intial="hidden"
+                transition={{duration: 1, type:'spring', stiffness: 150, yoyo: 10}}
+                animate = "visible">
+                    Welcome
+                </motion.h2>
                 <Navigation />
                 <Routes>
                     <Route path='/app' element={<Application updatePosts = {this.updatePosts} />}/>
